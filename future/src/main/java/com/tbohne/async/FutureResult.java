51218474bc08e7e5cbf3a5fd1573f04b2ed35b8a
MooingDuck <mooing_psycho_duck@yahoo.com>
2019-05-05 23:03:07 -0700

    Can query exception from FutureResult explicitly

--- future/src/main/java/com/tbohne/async/FutureResult.java
+++ future/src/main/java/com/tbohne/async/FutureResult.java
@@ -2,5 +2,6 @@ package com.tbohne.async;
 
 public interface FutureResult<R> {
 	boolean succeeded();
+	RuntimeException getThrownException();
 	R getNow(); //nonblocking, throws RuntimeException
 }
