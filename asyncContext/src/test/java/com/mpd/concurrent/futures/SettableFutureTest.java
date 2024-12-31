4a15e3a807e731c801961486ed2f08b8b3d8a81f
MPD <mooing_psycho_duck@hotmail.com>
2024-12-31 13:38:12 -0800

    first SettableFutureTest

---
+++ asyncContext/src/test/java/com/mpd/concurrent/futures/SettableFutureTest.java
@@ -0,0 +1,27 @@
+package com.mpd.concurrent.futures;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import com.mpd.concurrent.futures.Future.FutureNotCompleteException;
+import com.mpd.test.ErrorCollector;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SettableFutureTest {
+	@Rule public ErrorCollector collector = new ErrorCollector();
+
+	@Test public void construct_incomplete() {
+		SettableFuture<String> fut = new SettableFuture<>();
+		fut.end();
+
+		collector.checkThrows(FutureNotCompleteException.class, fut::getDone);
+		collector.checkThat(fut.isSuccessful(), equalTo(false));
+		collector.checkThat(fut.isDone(), equalTo(false));
+		collector.checkThat(fut.isCancelled(), equalTo(false));
+		collector.checkThrows(FutureNotCompleteException.class, fut::getException);
+		collector.checkThrows(UnsupportedOperationException.class, () -> fut.getDelay(TimeUnit.MILLISECONDS));
+	}
+}
