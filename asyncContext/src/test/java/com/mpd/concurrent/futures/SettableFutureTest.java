472109e656c457b5cb170859af71a896c83fd664
MPD <mooing_psycho_duck@hotmail.com>
2025-01-14 14:44:30 -0800

    Formatting

--- asyncContext/src/test/java/com/mpd/concurrent/futures/SettableFutureTest.java
+++ asyncContext/src/test/java/com/mpd/concurrent/futures/SettableFutureTest.java
@@ -18,8 +18,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-@RunWith(RobolectricTestRunner.class)
-public class SettableFutureTest {
+@RunWith(RobolectricTestRunner.class) public class SettableFutureTest {
 	@Rule public ErrorCollector collector = new ErrorCollector();
 
 	@Test public void construct_incomplete() {
