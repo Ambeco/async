27e4aa651119a4c2ba2799615677e29a92dc9b7b
MPD <mooing_psycho_duck@hotmail.com>
2025-01-20 08:39:56 -0800

    ErrorCollector added checkSucceeds(callable, matcher)
    Signed-off-by: MPD <mooing_psycho_duck@hotmail.com>

--- asyncContext/src/test/java/com/mpd/test/rules/ErrorCollector.java
+++ asyncContext/src/test/java/com/mpd/test/rules/ErrorCollector.java
@@ -1,11 +1,25 @@
 package com.mpd.test.rules;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertThrows;
 
+import java.util.concurrent.Callable;
 import org.hamcrest.Matcher;
 import org.junit.function.ThrowingRunnable;
 
 public class ErrorCollector extends org.junit.rules.ErrorCollector {
+
+	public <T> T checkSucceeds(Callable<T> callable, Matcher<T> matcher) {
+		return checkSucceeds("", callable, matcher);
+	}
+
+	public <T> T checkSucceeds(String reason, Callable<T> callable, Matcher<T> matcher) {
+		return checkSucceeds(() -> {
+			T value = callable.call();
+			assertThat(reason, value, matcher);
+			return value;
+		});
+	}
 
 	public <E extends Throwable> void checkThrows(
 			Class<E> expectedThrowable, ThrowingRunnable runnable, Matcher<E> matcher)
